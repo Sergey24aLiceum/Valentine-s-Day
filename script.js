6556c46436f0daed654841bffeff040e8be32f32
Sergey24aLiceum <24a_ksv@liceum.ztu.edu.ua>
2025-02-17 09:50:15 +0200

    +

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn_love_wishes").addEventListener("click",function() {
+//console.log(document.getElementById("btn_love_wishes"))
+document.getElementById("love-wishes").innerText="Let every day bring joy and warmth!";
+})
